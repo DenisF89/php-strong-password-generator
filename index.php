56806e082276e562c71ae39087602f55320aaa96
Denis89 <filippinidenis@gmail.com>
2026-04-12 21:56:38 +0200

    Milestone 3 - sessione

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<!-- Milestone 1
+<?php
+/*  
+Milestone 1
 Creare un form che invii in GET la lunghezza desiderata della password. 
 Una nostra funzione utilizzerà questo dato per generare una password casuale 
 (composta da lettere minuscole, maiuscole, numeri e/o simboli) della lunghezza specificata, da restituire all’utente.
@@ -7,12 +9,22 @@ Scirivamo tutta la logica ed il layout in un unico file index.php
 Milestone 2
 Verificato il corretto funzionamento del nostro codice, 
 spostiamo la logica in un file functions.php, che includeremo poi nella pagina principale.
--->
 
+Milestone 3 (BONUS)
+Invece di visualizzare la password generata nella stessa pagina (index.php), 
+effettuiamo un redirect ad una seconda pagina (result.php), dedicata proprio a mostrare il risultato. 
+Questa pagina riceverà la password che era stata generata tramite sessione e la mostrerà all’utente.
+*/
 
+session_start();
 
-<?php
 require_once 'functions.php';
+
+if (isset($_GET['length']) && $_GET['length'] != ""){
+    $_SESSION['password'] = generatePassword();
+    header('Location: result.php');
+}
+
 ?>
 
 
@@ -32,11 +44,6 @@ require_once 'functions.php';
     value=<?php echo $_GET['length']??""?>>
         <button type="submit">Genera Password</button>
 </form>
-
-<?php if (isset($_GET['length']) && $_GET['length'] != ""){
-   echo "<p>Password generata: ". generatePassword() ."</p>";
-   }
-?>
     
 </body>
 </html>
