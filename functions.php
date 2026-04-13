b2fee16f09146f989785bce4c30f1337ad2068b9
Denis89 <filippinidenis@gmail.com>
2026-04-13 10:42:02 +0200

    Milestone 4 - scelta di set di caratteri

--- functions.php
+++ functions.php
@@ -2,20 +2,34 @@
 
 function generatePassword() {
     $length = $_GET['length'] ?? 8; 
-    
-    $numbers = range('0', '9');                 // range() crea un array con i valori compresi tra quelli specificati.
-    $lower   = range('a', 'z');
-    $upper   = range('A', 'Z');
-    $symbols = str_split('!@#$%^&*?');          // str_split() divide una stringa in un array di caratteri.
+    $repeat = $_GET['repeat'] ?? 'si';
+    $selected = $_GET['characters'] ?? [];                      //array che contiene i tipi di caratteri selezionati dall'utente (numeri, lettere minuscole, lettere maiuscole, simboli)
+                                                   
+    $map = [                                                    //mappa che associa ogni tipo di carattere al relativo set di caratteri. E' un array associativo chiave(tipo)=>valore(array di caratteri).
+        'numbers' => range('0', '9'),                           // range() crea un array con i valori compresi tra quelli specificati.
+        'lower'   => range('a', 'z'),
+        'upper'   => range('A', 'Z'),
+        'symbols' => str_split('!@#$%^&*?')                     // str_split() divide una stringa in un array di caratteri.
+    ];
 
-    $characters = array_merge($numbers, $lower, $upper, $symbols);      //array_merge unisce più array in uno solo. 
+    $characters = [];
+
+    foreach ($selected as $type) {                              //per ogni tipo di carattere nell'array $selected
+        $characters = array_merge($characters, $map[$type]);    //prendo il set di caratteri corrispondente dalla mappa e lo unisco all'array $characters con array_merge().
+    }
 
     $password = [];
     for ($i = 0; $i < $length; $i++) {
-        $password[] = $characters[random_int(0, count($characters)-1)]; //prendo un carattere casuale dall'array. 
+        
+            $index = random_int(0, count($characters)-1);       //indice casuale dell'array
+            $password[] = $characters[$index];                  //prendo il carattere corrispondente all'indice e lo aggiungo alla password
+            
+            if ($repeat === 'no') {                             //se non sono ammesse ripetizioni
+                array_splice($characters, $index, 1);           //rimuovo il carattere dall'array $characters con array_splice()
+            }
     }
-    shuffle($password);                 // mescola i caratteri
-    return implode('', $password);      // converte l'array in stringa
+    shuffle($password);                                         //mescola i caratteri
+    return implode('', $password);                              //converte l'array in stringa
 }
 
 ?>
--- index.php
+++ index.php
@@ -14,6 +14,10 @@ Milestone 3 (BONUS)
 Invece di visualizzare la password generata nella stessa pagina (index.php), 
 effettuiamo un redirect ad una seconda pagina (result.php), dedicata proprio a mostrare il risultato. 
 Questa pagina riceverà la password che era stata generata tramite sessione e la mostrerà all’utente.
+
+Milestone 4 (BONUS)
+Gestire ulteriori parametri nel form per le password, dando la possibilità all’utente di specificare 
+quali set di caratteri possono essere ammessi nella password da generare, tra lettere maiuscole, lettere minuscole, numeri e simboli.
 */
 
 session_start();
@@ -42,7 +46,25 @@ if (isset($_GET['length']) && $_GET['length'] != ""){
     <label for="length">Lunghezza password:</label>
     <input type="number" id="length" name="length" min="8" max="20"
     value=<?php echo $_GET['length']??""?>>
-        <button type="submit">Genera Password</button>
+    <br>
+    <label for="repeat">Consenti ripetizioni di uno o più caratteri</label>
+    <input type="radio" id="repeat" name="repeat" value="si">
+    <label for="repeat">Si</label>
+    <input type="radio" id="repeat" name="repeat" value="no">
+    <label for="repeat">No</label>
+    <br>
+    <!-- creo un array (characters[]) che ha per valore i tipi di caratteri selezionati (checked) -->
+    <label for="characters">Caratteri ammessi:</label>
+    <input type="checkbox" id="numbers" name="characters[]" value="numbers">
+    <label for="numbers">Numeri</label>
+    <input type="checkbox" id="lower" name="characters[]" value="lower">
+    <label for="lower">Lettere minuscole</label>
+    <input type="checkbox" id="upper" name="characters[]" value="upper">
+    <label for="upper">Lettere maiuscole</label>
+    <input type="checkbox" id="symbols" name="characters[]" value="symbols">
+    <label for="symbols">Simboli</label>
+    <br>    
+    <button type="submit">Genera Password</button>
 </form>
     
 </body>
